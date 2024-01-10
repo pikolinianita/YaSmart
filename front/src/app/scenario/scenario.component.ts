414685baa94908952f4aae963c028bf5fa750e4c
pikolinianita <lukaszsobczak@yahoo.com>
2024-01-10 13:23:51 +0100

    Scenario a bit done

--- front/src/app/scenario/scenario.component.ts
+++ front/src/app/scenario/scenario.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { TaskComponent } from '../task/task.component';
+import { IconComponent } from "../icon/icon.component";
+import { Icon } from '../domain/icon';
 
 @Component({
-  selector: 'app-scenario',
-  standalone: true,
-  imports: [],
-  templateUrl: './scenario.component.html',
-  styleUrl: './scenario.component.css'
+    selector: 'app-scenario',
+    standalone: true,
+    templateUrl: './scenario.component.html',
+    styleUrl: './scenario.component.css',
+    imports: [TaskComponent, IconComponent]
 })
 export class ScenarioComponent {
 
+  public reward = new Icon("aa-bb-icon", 250, "monetization_on", "gold");
+  public reward2 = new Icon("aa-bb-icon", 10, "local_pizza","brown");
+  public title = "Kolorowanie Angulara"
 }
