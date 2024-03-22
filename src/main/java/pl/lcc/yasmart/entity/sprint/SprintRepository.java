58082371f7f7ed00c7893ada061316eb263e44d6
pikolinianita <pikolinian@gazeta.pl>
2024-03-22 14:49:10 +0100

    repo fix

--- src/main/java/pl/lcc/yasmart/entity/sprint/SprintRepository.java
+++ src/main/java/pl/lcc/yasmart/entity/sprint/SprintRepository.java
@@ -11,16 +11,17 @@ public interface SprintRepository extends CrudRepository<Sprint, UUID> {
     List<Sprint> findAllByOwner(Account account);
 
     @Query("""
-        SELECT new pl.lcc.yasmart.entity.SprintSummaryDTO
-        (s.id,
-        s.name,
-        s.state,
-        count (t.name),
+        SELECT new pl.lcc.yasmart.entity.sprint.SprintSummaryDTO
+        (s.id, s.name, s.state, count (t.name),
         -1
        )
         FROM Sprint s
-        JOIN tasks t on t.sprint = s
+        JOIN tasks t ON t.sprint = s
+        
+        GROUP BY s
         """)
+    //, t//where t.state='DONE'
     //(select count(t.name) where t.state='DONE' group by t.name)
+    // (select count(t.name) where t.state='DONE' group by t)
     List<SprintSummaryDTO> getAllProjectsSummary(Account account);
 }
